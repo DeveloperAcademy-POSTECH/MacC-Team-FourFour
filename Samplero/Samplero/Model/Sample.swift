654fe079b664697f423ef9e522a3dbf086d208b9
Miller <eotjd8721@naver.com>
2022-10-09 14:49:13 +0900

    [ADD] imageAsset & SampleModel & MockData

---
+++ Samplero/Samplero/Model/Sample.swift
@@ -0,0 +1,20 @@
+//
+//  Sample.swift
+//  Samplero
+//
+//  Created by DaeSeong on 2022/10/09.
+//
+
+import Foundation
+
+
+struct Sample {
+    let matName: String
+    let matPrice: String
+    let material: String
+    let thickness: String
+    let size: String
+    let maker: String
+}
+
+
