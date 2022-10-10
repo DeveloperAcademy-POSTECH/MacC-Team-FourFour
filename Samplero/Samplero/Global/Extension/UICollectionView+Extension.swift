bfa63528d0d620584c524f238fbc38ebef3a74df
JIW00NG <ipkjw2@gmail.com>
2022-10-10 15:24:38 +0900

    [ADD] add UICollectionView+Extension (#7)

---
+++ Samplero/Samplero/Global/Extension/UICollectionView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UICollectionView+Extension.swift
+//  Samplero
+//
+//  Created by JiwKang on 2022/10/10.
+//
+
+import Foundation
+import UIKit
+
+extension UICollectionView {
+    func dequeReusableCell<T: UICollectionViewCell>(withType cellType: T.Type, for indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withReuseIdentifier: T.className, for: indexPath) as? T else {
+            fatalError("Could not find cell with reuseID \(T.className)")
+        }
+        return cell
+    }
+    
+    func registor<T>(cell: T.Type, forCellReuseIdentifier reuseIdentifier: String = T.className) where T: UICollectionViewCell {
+        register(cell, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
