5cbcb8d164f8ce1bd8f606704b050c80e943d126
Miller <eotjd8721@naver.com>
2022-10-11 17:01:01 +0900

    [FIX] UICollectionView+extension

--- Samplero/Samplero/Global/Extension/UICollectionView+Extension.swift
+++ Samplero/Samplero/Global/Extension/UICollectionView+Extension.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 extension UICollectionView {
-    func dequeReusableCell<T: UICollectionViewCell>(withType cellType: T.Type, for indexPath: IndexPath) -> T {
+    func dequeueReusableCell<T: UICollectionViewCell>(withType cellType: T.Type, for indexPath: IndexPath) -> T {
         guard let cell = self.dequeueReusableCell(withReuseIdentifier: T.className, for: indexPath) as? T else {
             fatalError("Could not find cell with reuseID \(T.className)")
         }
         return cell
     }
     
-    func registor<T>(cell: T.Type, forCellReuseIdentifier reuseIdentifier: String = T.className) where T: UICollectionViewCell {
+    func register<T>(cell: T.Type, forCellReuseIdentifier reuseIdentifier: String = T.className) where T: UICollectionViewCell {
         register(cell, forCellWithReuseIdentifier: reuseIdentifier)
     }
 }
