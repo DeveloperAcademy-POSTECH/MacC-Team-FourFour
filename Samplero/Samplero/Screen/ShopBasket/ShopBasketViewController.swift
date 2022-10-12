e951cf35e30d4c7620ffadd876fbf0a58abf2dea
Miller <eotjd8721@naver.com>
2022-10-12 14:42:22 +0900

    [ADD] Shopbasket UI added

---
+++ Samplero/Samplero/Screen/ShopBasket/ShopBasketViewController.swift
@@ -0,0 +1,223 @@
+//
+//  ShopBasketViewController.swift
+//  Samplero
+//
+//  Created by DaeSeong on 2022/10/10.
+//
+
+import UIKit
+
+import SnapKit
+
+private enum Size {
+    static let noticeBackgroundHeight = 42.0
+    static let defaultOffset = 20.0
+    static let allButtonsBackgroundHeight = 51.0
+    static let orderButtonCorneradius = 14.0
+    static let orderButtonHeight = 64.0
+    static let buttonTextStackViewSpacing = 2.0
+    static let zPositionValue = 1.0
+    static let cellSpacing = 32.0
+    static let cellHeight = 80.0
+}
+
+class ShopBasketViewController: BaseViewController {
+
+
+    // MARK: - Properties
+    // View consists of noticeBackgroundView, allButtonsBackgroundView, shopBasketCollectionView
+    private let noticeBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray4
+        return view
+    }()
+
+    private let noticeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ⓘ 샘플은 하나씩만 구매할 수 있어요."
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        return label
+    }()
+
+    private let allButtonsBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+
+    private let allChoiceButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("  전체 선택", for: .normal)  // 띄어쓰기 논의해봐야 할듯
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        button.setTitleColor(.systemGray2, for: .normal)
+        button.setImage(UIImage(systemName: "square.fill"), for: .normal)
+        button.imageView?.tintColor = .systemGray3
+        return button
+    }()
+
+    private let allDeleteButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전체 삭제", for: .normal)  // 띄어쓰기 논의해봐야 할듯
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        button.setTitleColor(.systemGray2, for: .normal)
+        return button
+    }()
+
+    // orderButton consists of buttonTextStackView( buttonFirstLabel, buttonSecondLabel )
+    private let orderButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = Size.orderButtonCorneradius
+        button.layer.zPosition = Size.zPositionValue
+        return button
+    }()
+
+    private let buttonTextStackView: UIStackView = {
+        let view = UIStackView()
+        view.alignment = .center
+        view.axis = .vertical
+        view.distribution = .fillEqually
+        view.spacing = Size.buttonTextStackViewSpacing
+        return view
+    }()
+
+    private let buttonFirstLabel: UILabel = {
+        let label = UILabel()
+        label.text = "4개의 샘플" // FIXME: - 4 bold
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .white
+        return label
+    }()
+
+    private let buttonSecondLabel: UILabel = {
+        let label = UILabel()
+        label.text = "주문하기"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+
+    private let shopBasketFlowLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = Size.cellSpacing
+
+        return layout
+    }()
+
+    private lazy var shopBasketCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.shopBasketFlowLayout)
+        collectionView.register(cell: ShopBasketCollectionViewCell.self)
+        collectionView.backgroundColor = .systemBackground
+        return collectionView
+    }()
+
+
+    // MARK: - Life Cycle
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDelegation()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        hideNavBar()
+    }
+
+    override func render() {
+        view.addSubview(noticeBackgroundView)
+        noticeBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(Size.noticeBackgroundHeight)
+        }
+
+        noticeBackgroundView.addSubview(noticeLabel)
+        noticeLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(Size.defaultOffset)
+        }
+
+        view.addSubview(allButtonsBackgroundView)
+        allButtonsBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(noticeBackgroundView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(Size.allButtonsBackgroundHeight)
+        }
+        allButtonsBackgroundView.addSubview(allChoiceButton)
+        allChoiceButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(Size.defaultOffset) // 18인지 20인지 헷갈
+        }
+        allButtonsBackgroundView.addSubview(allDeleteButton)
+        allDeleteButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(Size.defaultOffset)
+        }
+
+        view.addSubview(orderButton)
+        orderButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(Size.defaultOffset)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(Size.orderButtonHeight)
+        }
+
+        orderButton.addSubview(buttonTextStackView)
+        buttonTextStackView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+        buttonTextStackView.addArrangedSubview(buttonFirstLabel)
+        buttonTextStackView.addArrangedSubview(buttonSecondLabel)
+
+        view.addSubview(shopBasketCollectionView)
+        shopBasketCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(allButtonsBackgroundView.snp.bottom)
+            make.leading.bottom.trailing.equalToSuperview()
+        }
+    }
+
+    override func configUI() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "장바구니"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+    // MARK: - Func
+
+
+    private func setDelegation() {
+        shopBasketCollectionView.dataSource = self
+        shopBasketCollectionView.delegate = self
+    }
+
+    private func hideNavBar() {
+        navigationController?.navigationBar.isHidden = false
+    }
+}
+
+
+// MARK: - UICollectionViewDataSource
+
+
+extension ShopBasketViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withType: ShopBasketCollectionViewCell.self, for: indexPath)
+        return cell
+    }
+}
+
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+
+extension ShopBasketViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        .init(width: collectionView.bounds.width, height: Size.cellHeight)
+    }
+}
---
+++ Samplero/Samplero/Screen/ShopBasket/View/Cell/ShopBasketCollectionViewCell.swift
@@ -0,0 +1,155 @@
+//
+//  ShopBasketCollectionViewCell.swift
+//  Samplero
+//
+//  Created by DaeSeong on 2022/10/12.
+//
+
+import UIKit
+
+
+private enum Size {
+    static let sampleImageSize = 80.0
+    static let defaultOffset = 20.0
+    static let secondaryOffset = 16
+}
+
+final class ShopBasketCollectionViewCell: BaseCollectionViewCell {
+
+
+    // MARK: - Properties
+    // Cell consists of checkBox,sampleImageView, verticalStackView
+
+
+    private let checkBox: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "square.fill"), for: .normal)
+        button.imageView?.tintColor = .systemGray3
+        return button
+    }()
+
+    private let sampleImageView: UIImageView =  {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "WhiteMarble")
+        return imageView
+    }()
+
+    // verticalStackView consists of firstHorizontalStackView, matNameLabel, thicknessLabel,samplePriceLabel
+    private let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 1.33
+        return stackView
+    }()
+
+    // firstHorizontalStackView consists of makerLabel,deleteButton
+    private let firstHorizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+
+    private let makerLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.text = "봄봄매트"
+        label.textColor = .label
+        return label
+    }()
+
+    private let deleteButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "xmark"), for: .normal)
+        button.imageView?.tintColor = .systemGray3
+        return button
+    }()
+
+    private let matNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        label.text = "화이트마블"
+        label.textColor = .label
+        return label
+    }()
+
+    private let thicknessLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.text = "두께: 4cm"
+        label.textColor = .label
+        label.alpha = 0.6
+        return label
+    }()
+
+    private let samplePriceLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        label.text = "0원"
+        label.textColor = .systemBlue
+        label.textAlignment = .right
+        return label
+    }()
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+    // MARK: - Func
+
+
+    override func render() {
+
+        contentView.addSubview(checkBox)
+        checkBox.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview().offset(Size.defaultOffset)
+            make.size.equalTo(Size.defaultOffset)
+        }
+
+        contentView.addSubview(sampleImageView)
+        sampleImageView.snp.makeConstraints { make in
+            make.size.equalTo(Size.sampleImageSize)
+            make.leading.equalTo(checkBox.snp.trailing).offset(Size.secondaryOffset)
+            make.top.bottom.equalToSuperview()
+        }
+
+        contentView.addSubview(verticalStackView)
+        verticalStackView.snp.makeConstraints { make in
+            make.leading.equalTo(sampleImageView.snp.trailing).offset(Size.secondaryOffset)
+            make.top.bottom.equalToSuperview()
+            make.trailing.equalToSuperview().inset(Size.defaultOffset)
+        }
+
+        verticalStackView.addArrangedSubview(firstHorizontalStackView)
+        firstHorizontalStackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+        }
+
+        firstHorizontalStackView.addArrangedSubview(makerLabel)
+        firstHorizontalStackView.addArrangedSubview(deleteButton)
+        deleteButton.snp.makeConstraints { make in
+            make.size.equalTo(Size.defaultOffset)
+        }
+
+        verticalStackView.addArrangedSubview(matNameLabel)
+        verticalStackView.addArrangedSubview(thicknessLabel)
+        verticalStackView.addArrangedSubview(samplePriceLabel)
+        samplePriceLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+        }
+    }
+
+
+}
+
