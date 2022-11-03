28d76da05a9f614067e0d1727b79829ead973d5b
Taekyun Kim <taekkim@student.42seoul.kr>
2022-11-03 22:20:04 +0900

    [REFACTOR]: adopt input output pattern

--- Samplero/Samplero/Screen/EstimateHistory/ViewModel/EstimateHistoryViewModel.swift
+++ Samplero/Samplero/Screen/EstimateHistory/ViewModel/EstimateHistoryViewModel.swift
@@ -3,26 +3,42 @@
 //  Samplero
 //
 //  Created by JiwKang on 2022/10/10.
+//  Modified by Cozy on 2022/11/03.
 //
 
 import Foundation
 
+import RxCocoa
 import RxSwift
 
-class EstimateHistoryViewModel {
+class EstimateHistoryViewModel: ViewModelType {
     
     // MARK: - Properties
     
     let db = DBHelper.shared
-    
     var disposeBag = DisposeBag()
-    
-    let estimateHistorySubject: BehaviorSubject<[EstimateHistory]>
-    
-    init () {
-        var estimateHistories: [EstimateHistory] = []
-        estimateHistories = db.getEstimateHistories()
-        
-        estimateHistorySubject = BehaviorSubject(value: estimateHistories)
+
+    struct Input {
+        let viewWillAppear: ControlEvent<Bool>
+        let itemSelected: ControlEvent<IndexPath>
+    }
+
+    struct Output {
+        let isEmptyCell: Observable<Bool>
+        let estimateHistorySubject: Observable<[EstimateHistory]>
+        let tappedItem: Observable<IndexPath>
+    }
+
+    func transform(input: Input) -> Output {
+        let estimateHistorySubject: BehaviorSubject<[EstimateHistory]> = BehaviorSubject(value: db.getEstimateHistories())
+
+        let isEmptyCell = input.viewWillAppear
+            .withLatestFrom(estimateHistorySubject)
+            .map { $0.count > 0 ? true : false }
+
+        let tappedItem = input.itemSelected
+            .asObservable()
+
+        return Output(isEmptyCell: isEmptyCell, estimateHistorySubject: estimateHistorySubject, tappedItem: tappedItem)
     }
 }
